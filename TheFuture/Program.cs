47ef76982ef050906925967e3d04bac46ebbb2e3
Khalid Abuhakmeh <khalidabuhakmeh@gmail.com>
2021-05-27 10:46:43 -0400

    Add global using static for Console

--- TheFuture/Program.cs
+++ TheFuture/Program.cs
@@ -1,22 +1,22 @@
 ﻿// const string interpolation
 const string s1 = $"abc";
 const string s2 = $"{s1}edf";
-Console.WriteLine(s2);
+WriteLine(s2);
 
 // default Tuple deconstruction
 var (x, y) = default((int, bool));
-Console.WriteLine($"({x}, {y})");
+WriteLine($"({x}, {y})");
 
 // lambda target typing to Delegate
 void Method(Delegate hello) {
-    Console.WriteLine(hello.DynamicInvoke());
+    WriteLine(hello.DynamicInvoke());
 }
 
 Method(() => "Hello");
 
 // record struct
 var person = new Person("Khalid");
-Console.WriteLine(person);
+WriteLine(person);
 
 record struct Person(string Name);
 
